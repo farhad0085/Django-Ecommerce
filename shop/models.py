4bfead1ecb32079f2a546f85cb934f24d21405c5
Farhad Hossain <farhadhossain0085@gmail.com>
2020-09-02 02:57:55 +0600

    things not working

--- shop/models.py
+++ shop/models.py
@@ -11,18 +11,18 @@ class Product(models.Model):
     product_name = models.CharField(max_length=260)
     product_price = models.IntegerField()
     product_description = models.TextField()
-    product_picture = models.ImageField(upload_to='profile_pics', default='default-product.jpg')
+    product_picture = models.ImageField(upload_to='product_pictures', default='default-product.jpg')
     categories = models.ManyToManyField(ProductCategory)
     
     def __str__(self):
         return f"{self.id} - {self.product_name}"
 
 class ProductReview(models.Model):
-    author_name = models.CharField(max_length=260)
+    author_name = models.CharField(max_length=30)
     author_email = models.EmailField()
     review_description = models.TextField()
     review_score = models.IntegerField()
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     
     def __str__(self):
-        return f"{self.id} - {self.product_name}"
+        return f"{self.author_name} - {self.review_score}"
--- shop/views.py
+++ shop/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Product
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello")
 
-def shop_home(request, *args, **kwargs):
-    return render(request, template_name='shop/shop_home.html')
+def shop_home(request):
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, template_name='shop/shop_home.html', context=context)
 
 def shop_item_details(request, *args, **kwargs):
     return render(request, template_name='shop/shop_item_details.html')
