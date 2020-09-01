dc0f6748a439c43073281496995e1dfa60bacbe7
Farhad Hossain <farhadhossain0085@gmail.com>
2020-09-02 00:40:07 +0600

    INit

--- myecommerce/urls.py
+++ myecommerce/urls.py
@@ -1,4 +1,4 @@
-"""mydjango URL Configuration
+"""myecommerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
